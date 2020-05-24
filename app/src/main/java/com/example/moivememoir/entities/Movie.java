0994abc552e9c03d9eb0ebd7cb48a372ee029ae3
Fabio <Yang>
2020-05-24 21:25:12 +1000

    added secretes file, yep gitignore worked

---
+++ app/src/main/java/com/example/moivememoir/entities/Movie.java
@@ -0,0 +1,10 @@
+package com.example.moivememoir.entities;
+
+import java.util.Date;
+
+public class Movie {
+    private String name;
+    private Date releaseDate;
+    private String detail;
+    private String imageLink;
+}
